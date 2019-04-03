4a5e04eaa066e936f49bd3eb4c9e2d0af1d4fe0f
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-04-04 00:27:51 +0900

    after review from Alex Melnikov + @Autowired over constructor

--- L5CRUD_Spring/src/main/java/com/example/test/controller/UserController.java
+++ L5CRUD_Spring/src/main/java/com/example/test/controller/UserController.java
@@ -1,35 +1,31 @@
 package com.example.test.controller;
 
-import com.example.test.model.Role;
-import com.example.test.model.User;
 import com.example.test.service.RoleService;
 import com.example.test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
     private UserService userService;
-    @Autowired
     private RoleService roleService;
-    @Autowired
     private PasswordEncoder passwordEncoder;
 
+    // All people must have! annotation @Autowired over construcor
+    @Autowired
+    public UserController(UserService userService,RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.roleService = roleService;
+        this.userService = userService;
+    }
 
     @GetMapping(value = {"/welcome", "/"})
     public String welcome(
