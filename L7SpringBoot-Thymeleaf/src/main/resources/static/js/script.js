55cb16db8b5273d3d2b22086ec863ac1dbd02c0f
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-04-11 00:40:01 +0900

    L8 Add jQuery MIN !

---
+++ L7SpringBoot-Thymeleaf/src/main/resources/static/js/script.js
@@ -0,0 +1,17 @@
+
+$('#userModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var userid = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    //  $('#editUserForm').load('/admin/ediuser?id=' + userid)
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + userid)
+    modal.find('.modal-body').load('edituser?id=' + userid)
+    //
+    // $('#editUserForm').html('<div>AAAAAAAAAAAAAAAAAA</div>')
+
+
+})
+
+
