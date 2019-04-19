169ae15b20b64134a085d12401c4222a11035a8a
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-04-20 00:31:10 +0900

    L10 REST Client CRUD

--- L7SpringBoot-Thymeleaf/src/main/resources/static/js/rest_client.js
+++ L7SpringBoot-Thymeleaf/src/main/resources/static/js/rest_client.js
@@ -1,7 +1,79 @@
 $(document).ready(function () {
     listUsers();
     getRoles();
-});
+    //REST Update user by id on Click() button
+    $('#updateUser').click(function () {
+        var form = $('#updateForm');
+
+        var obj = {};
+
+        obj['id'] = +form.find('#userid').val();
+        obj['name'] = form.find('#Username').val();
+        obj['password'] = form.find('#Password').val();
+        obj['email'] = form.find('#email').val();
+        obj['description'] = form.find('#Description').val();
+        obj['roles'] = [];
+        form.find('[id^="role_id"]:checked').each(function () {
+            var role = {};
+            role.id = +$(this).val();
+
+            obj['roles'].push(role);
+
+        });
+        var json = JSON.stringify(obj);
+        $.ajax({
+            url: "/rest/user/" + form.find('#userid').val(),
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: json,
+            datatype: "json",
+            success: function (data) {
+                $(".modal").modal("hide");
+                listUsers();
+                getRoles();
+            }
+
+        });
+    });
+//Cancel submit action and REST request
+    $('#newuser-btn').submit(function () {
+        return false;
+    });
+
+    $('#newuser-btn').click(function () {
+        var form = $('#newuserform');
+        var obj = {};
+
+        obj['name'] = form.find('#Username').val();
+        obj['password'] = form.find('#Password').val();
+        obj['email'] = form.find('#email').val();
+        obj['description'] = form.find('#Description').val();
+        obj['roles'] = [];
+        form.find('[id^="role_id"]:checked').each(function () {
+            var role = {};
+            role.id = +$(this).val();
+
+            obj['roles'].push(role);
+
+        });
+        var json = JSON.stringify(obj);
+        $.ajax({
+            url: "/rest/user/",
+            type: "PUT",
+            contentType: "application/json; charset=utf-8",
+            data: json,
+            datatype: "json",
+            success: function (data) {
+                console.log(data);
+                listUsers();
+                getRoles();
+            }
+        });
+    });
+
+
+})
+;
 
 
 $('#userModal').on('show.bs.modal', function (event) {
@@ -18,9 +90,9 @@ $('#userModal').on('show.bs.modal', function (event) {
         modal.find('#Username').val(data.name);
         modal.find('#Password').val(data.password);
         modal.find('#email').val(data.email);
-        // modal.find('#listRoles').replaceWith(rolesstr);
         checkUserRoles(data.roles);
         modal.find('#description').val(data.description);
+        modal.find('#deleteuserbtn').attr('onclick','deleteUser(' + data.id +')');
     });
 
 })
@@ -47,43 +119,63 @@ function listUsers() {
 
             listUsers += '</tr>';
         });
-        $("#listusers").replaceWith('<tbody>' + listUsers + '</tbody>');
+        $("#listusers").replaceWith('<tbody id="listusers">' + listUsers + '</tbody>');
     });
 }
 
-function getRoles(){
+function getRoles() {
     $.getJSON('/rest/role', function (data) {
-        $('#listRoles').replaceWith(rolesForForm(data));
+        var l = $('.listRoles').length;
+        $('.listRoles').each(function () {
+            $(this).html(rolesForForm(data));
+        });
     });
-
 }
-function rolesForForm(allroles){
+
+function rolesForForm(allroles) {
     var listRoles = '';
     $.each(allroles, function (index, value) {
-        listRoles += '<div class="input-group mb-3" >' +
-        '<div class="input-group-prepend"> '+
-        '<div class="input-group-text">' +
-        '<input type="checkbox" id="role_id_' + value.id + '" name="listRoles"';
+        listRoles += '<div class="input-group mb-3 "   >' +
+            '<div class="input-group-prepend"> ' +
+            '<div class="input-group-text">' +
+            '<input type="checkbox" id="role_id_' + value.id + '" name="listRoles" value=' + value.id;
 
-        listRoles += 'aria-label="Checkbox for following text input"> '+
-        '</div>' +
-        '</div>' +
-        '<input type="text" class="form-control" aria-label="Text input with checkbox"';
+        listRoles += ' aria-label="Checkbox for following text input"> ' +
+            '</div>' +
+            '</div>' +
+            '<input type="text" class="form-control" aria-label="Text input with checkbox"';
         listRoles += ' value="' + value.description + ' (' + value.name + ')"';
         listRoles += ' readonly> </div>';
 
-        });
+    });
     return listRoles;
 }
 
-function checkUserRoles(userRoles){
+function checkUserRoles(userRoles) {
     var form = $('#editUserForm');
-    form.find('input[type=checkbox]').each(function() {
+    form.find('input[type=checkbox]').each(function () {
         this.checked = false;
     });
 
-    $.each(userRoles, function(index, value){
+    $.each(userRoles, function (index, value) {
         form.find('#role_id_' + value.id).prop('checked', true);
     });
+}
 
+//
+function deleteUser(id){
+
+
+        $.ajax({
+            url: "/rest/user/"+id,
+            type: "DELETE",
+            contentType: "application/json; charset=utf-8",
+            datatype: "json",
+            success: function (data) {
+                $(".modal").modal("hide");
+                console.log(data);
+                listUsers();
+                getRoles();
+            }
+        });
 }
